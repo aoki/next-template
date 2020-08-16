841adc357fc0d5b7619124d1292c0ac7a7668c79
aoki <ringohub@gmail.com>
2020-08-16 18:03:12 +0900

    Add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
+import Head from 'next/head'
+
+const HomePage = () => {
+  return (
+    <>
+      <Head>
+        <title>Next App Template</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>Welcome to Next.js!</div>
+      </main>
+    </>
+  )
 }
 
 export default HomePage
